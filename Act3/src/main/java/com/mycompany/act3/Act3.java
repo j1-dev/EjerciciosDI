8a4ec061e8cb0d62d5193c00557445a6f409dd80
j1-dev <aephyxen@gmail.com>
2023-10-06 10:18:11 +0200

    lol

---
+++ Act3/src/main/java/com/mycompany/act3/Act3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.act3;
+
+/**
+ *
+ * @author j1
+ */
+public class Act3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
