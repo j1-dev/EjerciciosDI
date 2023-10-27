ff57b9099caba3669e4ffb51f476fb6dde08c9e1
j1-dev <aephyxen@gmail.com>
2023-10-27 09:53:16 +0200

    Started practica2Ud2

---
+++ Practica2Ud2/src/main/java/com/gestionhotel/dto/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gestionhotel.dto;
+
+/**
+ *
+ * @author j1
+ */
+class Persona {
+    private String nombre;
+    private int numTlf;
+    private String direccion;
+}
---
+++ Practica2Ud2/src/main/java/com/mycompany/practica2ud2/Practica2Ud2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica2ud2;
+
+/**
+ *
+ * @author j1
+ */
+public class Practica2Ud2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
